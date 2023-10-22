6c711fa28f31af1a1677a53b0c012c4038ed11eb
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 14:48:58 +0530

    Updates

---
+++ hotel-service/src/main/java/com/anjali/hs/service/custom/HotelService.java
@@ -0,0 +1,8 @@
+package com.anjali.hs.service.custom;
+
+import com.anjali.hs.dto.HotelDTO;
+import com.anjali.hs.service.SuperService;
+
+
+public interface HotelService extends SuperService<HotelDTO,String> {
+}
---
+++ hotel-service/src/test/java/com/anjali/hs/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjali.hs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
