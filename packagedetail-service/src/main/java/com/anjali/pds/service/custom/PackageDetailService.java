a1eca61f764a790785b3d5949ce6c5cd36ed57c6
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 14:59:24 +0530

    Updates

---
+++ packagedetail-service/src/main/java/com/anjali/pds/service/custom/PackageDetailService.java
@@ -0,0 +1,8 @@
+package com.anjali.pds.service.custom;
+
+
+import com.anjali.pds.dto.PackageDetailDTO;
+import com.anjali.pds.service.SuperService;
+
+public interface PackageDetailService extends SuperService<PackageDetailDTO,String> {
+}
---
+++ packagedetail-service/src/test/java/com/anjali/pds/PackagedetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjali.pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagedetailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
