0727ebb0d6151422957a9369aa7447c9c118d8ca
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 14:32:49 +0530

    Updates

---
+++ packagedetail-service/src/main/java/com/anjali/pds/interfaces/GuideControllerInterface.java
@@ -0,0 +1,14 @@
+package com.anjali.pds.interfaces;
+
+import com.anjali.pds.dto.GuideDTO;
+import com.anjali.pds.response.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "GUIDE-SERVICE")
+public interface GuideControllerInterface {
+    @GetMapping(path = "/search", params = "guideId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public GuideDTO getGuideDTO(@RequestParam("guideId") String guideId);
+}
