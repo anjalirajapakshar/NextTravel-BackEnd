da8ed8dbce4e63e70a05dd61c934e6d0370614ff
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 21:17:29 +0530

    Updates

---
+++ payment-service/src/main/java/com/anjali/ps/service/custom/PaymentService.java
@@ -0,0 +1,7 @@
+package com.anjali.ps.service.custom;
+
+import com.anjali.ps.dto.PaymentDTO;
+import com.anjali.ps.service.SuperService;
+
+public interface PaymentService extends SuperService<PaymentDTO,String> {
+}
