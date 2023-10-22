1af02f71e8e4a60cd6da8d2d70a8e1db7ad21eee
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 15:08:54 +0530

    Updates

---
+++ package-service/src/main/java/com/anjali/ps/service/custom/PackageService.java
@@ -0,0 +1,7 @@
+package com.anjali.ps.service.custom;
+
+import com.anjali.ps.dto.PackagesDTO;
+import com.anjali.ps.service.SuperService;
+
+public interface PackageService extends SuperService<PackagesDTO,String> {
+}
