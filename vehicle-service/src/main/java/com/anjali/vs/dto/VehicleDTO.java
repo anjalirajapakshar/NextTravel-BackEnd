5a9d2670110f39cfc4b463b2e42046a9c58c6e95
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 21:26:20 +0530

    Updates

---
+++ vehicle-service/src/main/java/com/anjali/vs/dto/VehicleDTO.java
@@ -0,0 +1,28 @@
+package com.anjali.vs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VehicleDTO implements SuperDTO, Serializable {
+    private String vehicleID;
+    private String vehicleBrand;
+    private String vehicleCategory;
+    private String fuelType;
+    private String hybridOrNot;
+    private String fuelUsage;
+    private String vehicleImage;
+    private String seatCapacity;
+    private String vehicleType;
+    private String transmissionType;
+    private String driversName;
+    private String driversContactNumber;
+    private String driverLicenseImageLocation;
+
+    private String packageId;
+}
