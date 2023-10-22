4ec07c0e4d6ddecd2edf2657bdf21dfa8690aff0
anjali <anjalirajapakshaar@gmail.com>
2023-10-22 15:06:06 +0530

    Updates

---
+++ package-service/src/main/java/com/anjali/ps/dto/PackagesDTO.java
@@ -0,0 +1,17 @@
+package com.anjali.ps.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PackagesDTO implements Serializable,SuperDTO{
+    private String packageId;
+    private String packageCategory;
+    private String vehicleCategory;
+    private String hotelCategory;
+}
