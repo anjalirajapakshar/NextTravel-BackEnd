35f6390ef3f63fda4795f9d2560942bfa88be5fe
anjali <anjalirajapakshaar@gmail.com>
2023-10-25 22:31:18 +0530

    Security Updates

---
+++ hotel-service/src/main/java/com/anjali/hs/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.anjali.hs.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll() // Accessible to everyone
+                .antMatchers("/**").hasAnyAuthority("HA","PDA","VA","PA","UA") // Requires ROLE_ADMIN authority
+//                .antMatchers("/**").hasAuthority("PDA") // Requires ROLE_ADMIN authority
+                .anyRequest().authenticated() // Any other request requires authentication
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
