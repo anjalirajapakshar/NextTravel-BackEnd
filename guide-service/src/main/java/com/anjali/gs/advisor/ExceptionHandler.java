22e7e944f398d7d80db2b6630cd45d613cec3f38
anjali <anjalirajapakshaar@gmail.com>
2023-10-27 21:58:46 +0530

    Exceptions handler Updates

---
+++ guide-service/src/main/java/com/anjali/gs/advisor/ExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.anjali.gs.advisor;
+
+
+import com.anjali.gs.response.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+@Component
+public class ExceptionHandler {
+    @Autowired
+    private Response response;
+    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
+    public Response handleExceptions(Exception exception ){
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessage("Server threw an exception : "+exception.getLocalizedMessage());
+        response.setData(null);
+        return response;
+
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({MethodArgumentNotValidException.class})
+    public Response handleExceptions(MethodArgumentNotValidException exception){
+        HashMap<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach((fieldError -> {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }));
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessage("Server threw an exception : "+exception.getLocalizedMessage());
+        response.setData(errors);
+        return response;
+
+    }
+
+
+}
